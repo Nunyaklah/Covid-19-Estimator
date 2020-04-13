a6e6cfd1c709d07dee0ae8b3e24f65ef53f4188b
Nunya <nunyaklah@gmail.com>
2020-04-13 23:22:07 +0000

    Testing new changes

--- src/estimator.js
+++ src/estimator.js
@@ -1,63 +1,28 @@
 const covid19ImpactEstimator = (data) => {
-  const input = data;
-  const estimator = {
-    data: {
-      region: {
-        name: 'Africa',
-        avgAge: 19.7,
-        avgDailyIncomeInUSD: 4,
-        avgDailyIncomePopulation: 0.73
-      },
-      periodType: 'days',
-      timeToElapse: 38,
-      reportedCases: input,
-      population: 92931687,
-      totalHospitalsBeds: 678874
-    },
-    estimate: {
-      impact: {
-        currentlyInfected: 0,
-        infectionsByRequestedTime: 0,
-        severeCasesByRequestedTime: 0,
-        hospitalBedsByRequestedTime: 0,
-        casesForVentilatorsByrequestedTime: 0,
-        dollarsInFlight: 0
-      },
-      severeImpact: {
-        currentlyInfected: 0,
-        infectionsByRequestedTime: 0,
-        severeCasesByRequestedTime: 0,
-        hospitalBedsByRequestedTime: 0,
-        casesForVentilatorsByrequestedTime: 0,
-        dollarsInFlight: 0
-      }
-    }
-  };
-  function currentlyInfectedEstimate() {
-    const estimate = estimator.data.reportedCases * 10;
-    estimator.estimate.impact.currentlyInfected = estimate;
+  const { name, avgAge, avgDailyIncomeUSD, avgDailyIncomePopulation } = data;
+  const {
+    periodType,
+    timeToElaspe,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  } = data.region;
+
+  const impact = {};
+  const severeImpact = {};
+
+  let period;
+  if (periodType === 'months') {
+    period = timeToElaspe * 30;
+  } else if (periodType === 'weeks') {
+    period = timeToElaspe * 7;
+  } else {
+    period = periodType;
   }
 
-  function currentlyInfectedEstimateSevere() {
-    const estimate = estimator.data.reportedCases * 50;
-    estimator.estimate.severeImpact.currentlyInfected = estimate;
-  }
+  impact.currentlyInfected = reportedCases * 10;
+  severeImpact.currentlyInfected = reportedCases * 50;
 
-  function infectionsByRequestedTime() {
-    const estimate = estimator.estimate.impact.currentlyInfected * 512;
-    estimator.estimate.impact.infectionsByRequestedTime = estimate;
-  }
-
-  function infectionsByRequestedTimeSevere() {
-    const estimate = estimator.estimate.severeImpact.currentlyInfected * 512;
-    estimator.estimate.severeImpact.infectionsByRequestedTime = estimate;
-  }
-  infectionsByRequestedTime();
-  currentlyInfectedEstimateSevere();
-  currentlyInfectedEstimate();
-  infectionsByRequestedTimeSevere();
+  return { data, impact, severeImpact };
 };
-
-covid19ImpactEstimator(2747);
-
-export default covid19ImpactEstimator;
+// export default covid19ImpactEstimator;
